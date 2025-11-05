f6908ec52e5716c6ab65125eada363122f160c23
Kevin Roleke <kevin@zerogon.consulting>
2025-11-04 19:42:25 -0800

    index page

--- Sources/server/main.swift
+++ Sources/server/main.swift
@@ -3,6 +3,15 @@ import Mustache
 import Foundation
 let library = try await MustacheLibrary(directory: "Resources")
 
+struct HTML: ResponseGenerator {
+    let html: String
+
+    public func response(from request: Request, context: some RequestContext) throws -> Response {
+        let buffer = ByteBuffer(string: self.html)
+        return .init(status: .ok, headers: [.contentType: "text/html"], body: .init(byteBuffer: buffer))
+    }
+}
+
 let router = Router()
 router.get("/get-udid.mobileconfig") { request, _ -> String in
     let requestUUID = UUID().uuidString
@@ -16,6 +25,9 @@ router.get("/register-udid/:uuid") { request, ctx -> String in
     let uuid = try ctx.parameters.require("uuid", as: String.self)
     print(uuid)
     return uuid
+}
+router.get("/") { request, ctx -> HTML in
+    return HTML(html: library.render([], withTemplate: "index") ?? String("abject failure"))
 }
 // create application using router
 let app = Application(
