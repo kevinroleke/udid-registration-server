a1607db4de4ff816c66d8c5000873efdbb528d5c
Kevin Roleke <kevin@zerogon.consulting>
2025-11-04 20:36:52 -0800

    sign mobile config on the fly

--- Sources/server/main.swift
+++ Sources/server/main.swift
@@ -12,14 +12,44 @@ struct HTML: ResponseGenerator {
     }
 }
 
+func signMobileConfig(mobileconfig: String) throws -> Hummingbird.ByteBuffer {
+    let fileManager = FileManager.default
+    let randomUUID = UUID().uuidString
+    let url = URL( fileURLWithPath: "./Temp/\(randomUUID)" )
+    let signedURL = URL( fileURLWithPath: "./Temp/\(randomUUID).signed" )
+
+    try mobileconfig.write(to: url, atomically: true, encoding: .utf8)
+
+    let task = Process()
+
+    task.arguments = ["smime", "-sign", "-in", "./Temp/\(randomUUID)",
+  "-out", "./Temp/\(randomUUID).signed",
+  "-signer", "../your-pub-cert.pem",
+  "-inkey", "../your-priv-key.pem",
+  "-certfile", "../your_cert.pem",
+  "-outform", "der", "-nodetach"]
+    task.launchPath = "/opt/homebrew/bin/openssl"
+    task.standardInput = nil
+    try task.run()
+    task.waitUntilExit()
+
+    let buf = Hummingbird.ByteBuffer(data: try Data(contentsOf: signedURL) )
+
+    try fileManager.removeItem(at: signedURL)
+    try fileManager.removeItem(at: url)
+
+    return buf
+}
+
 let router = Router()
-router.get("/get-udid.mobileconfig") { request, _ -> String in
+router.get("/get-udid.mobileconfig") { request, _ -> Hummingbird.ByteBuffer in
     let requestUUID = UUID().uuidString
     let mobileconfig = library.render([
         "uuid": requestUUID,
         "server": "https://udid.zerogon.consulting",
     ], withTemplate: "get-udid.mobileconfig")
-    return mobileconfig ?? String("abject failure")
+
+    return try! signMobileConfig(mobileconfig: mobileconfig!)
 }
 router.get("/register-udid/:uuid") { request, ctx -> String in
     let uuid = try ctx.parameters.require("uuid", as: String.self)
